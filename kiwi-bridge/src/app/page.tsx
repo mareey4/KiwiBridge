7dfcbb12b2e7336d74bfd583f7af11ad07e4d770
Maria <mp@Marias-Laptop.local>
2024-06-08 15:33:12 +1200

    chore: adding mui packages

--- kiwi-bridge/src/app/page.tsx
+++ kiwi-bridge/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
 
-    <h1></h1>ello world;
+    ello world;
     </div>
   );
 }
---
+++ kiwi-bridge/src/app/resource/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <Typography variant="h1" component="h2">
+        h1. Heading
+        </Typography>
+    );
+  }
+  
