ba3f2683cd55c0158401f2d432187a4e3b0c811f
Alyssa <alyssapausanos1449@gmail.com>
2024-06-08 17:35:13 +1200

    add news list

--- kiwi-bridge/src/app/resource/page.tsx
+++ kiwi-bridge/src/app/resource/page.tsx
@@ -1,24 +1,19 @@
-  import * as React from 'react';
-import Box from '@mui/material/Box';
-import { Container, Paper, Typography } from '@mui/material';
-import BasicTabs from '@/components/tabs';
-import LabelBottomNavigation from '@/components/bottomNavigation';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Container, Paper, Typography } from "@mui/material";
+import BasicTabs from "@/components/tabs";
+import LabelBottomNavigation from "@/components/bottomNavigation";
 
 export default function BoxBasic() {
   return (
     <Box component="section">
-        <Container fixed sx={{paddingTop:"30px"}}>
+      <Container fixed sx={{ paddingTop: "30px" }}>
         <Typography variant="h3" component="h4">
-            RESOURCE
-        </Typography>    
-        <BasicTabs>
-        </BasicTabs>
-        <LabelBottomNavigation>
-
-        </LabelBottomNavigation>
-        </Container>
-
+          RESOURCE
+        </Typography>
+        <BasicTabs></BasicTabs>
+        <LabelBottomNavigation>{}</LabelBottomNavigation>
+      </Container>
     </Box>
   );
 }
--- kiwi-bridge/src/components/nzHerald.tsx
+++ kiwi-bridge/src/components/nzHerald.tsx
@@ -1,20 +1,41 @@
-import Image from 'next/image'
-import nzHerald from '../../public/nzHerald1.png'
-import { Paper } from '@mui/material'
+import Image from "next/image";
+import nzHerald from "../../public/nzHerald1.png";
+import { Paper } from "@mui/material";
 
 export default function NzHerald() {
-    return(
-        <div>
+  interface Item {
+    id: number;
+    content: string;
+  }
+  const itemElements: Item[] = [
+    {
+      id: 1,
+      content: "lorem",
+    },
+    { id: 2, content: "lorem" },
+    { id: 3, content: "lorem" },
+    {
+      id: 4,
+      content: "lorem",
+    },
+    { id: 5, content: "lorem" },
+  ];
+  return (
+    <div>
+      <ul>
+        {itemElements.map((item) => (
+          <>
             <Paper elevation={3}>
-            <Image
-            src={nzHerald.src}
-            width={200}
-            height={150}
-            alt="Picture of a world"/>
-        
+              <Image
+                src={nzHerald.src}
+                width={200}
+                height={150}
+                alt="Picture of a world"
+              />
             </Paper>
-            
-
-        </div>
-    )
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 }
